2b9390890a1e042b378ba0c189cc7809792e3c39
Sijun Liu <sijliu@microsoft.com>
2017-10-11 00:00:13 -0700

    add tslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
+var tslint = require("gulp-tslint");
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
-    return tsProject.src()
+gulp.task("tslint", () =>
+    gulp.src('src/**/*.ts')
+        .pipe(tslint())
+        .pipe(tslint.report())
+);
+
+gulp.task("compile", ["tslint"], () => {
+    tsProject.src()
         .pipe(tsProject())
         .js.pipe(gulp.dest("dist"));
 });
+
+gulp.task("default", ["compile"]);
