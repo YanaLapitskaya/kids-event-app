ce30b6e48ef03a388604ae25c98ba32a7a5654d3
Lapitskaya Yana <Lapitskaya_YV@itd.iba.by>
2018-04-19 17:34:18 +0300

    chnages client controller

--- src/controllers/ClientCtrl.ts
+++ src/controllers/ClientCtrl.ts
@@ -6,48 +6,50 @@ export default class ClientRoutes {
     static getAllClients(req: Request, res: Response, next: NextFunction) {
         ClientRepo.getAllClients()
             .then((result) => {
-                res.json(result);
+                res.status(200).send({clients: result.map((empl) => empl.dataValues)});
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Fetch All Clients failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static getClientById(req: Request, res: Response) {
         ClientRepo.getClientById(req.params.id)
-            .then((result) => res.json(result))
-            .catch((err) => {
-                apiErrorHandler(err, req, res, `Client ${req.params.id} not found.`);
+            .then((client) => {
+                res.status(200).send({client: client});
+            })
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static addClient(req: Request, res: Response) {
         ClientRepo.createClient(req.body)
-            .then((result) => {
-                res.json(result);
+            .then((client: any) => {
+                res.status(200).send({client: client});
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Add Client failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static editClient(req: Request, res: Response) {
         ClientRepo.updateClient(req.params.id, req.body)
             .then((result) => {
-                res.json(result);
+                res.sendStatus(200);
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Edit Client failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static deleteClient(req: Request, res: Response) {
         ClientRepo.deleteClient(req.params.id)
             .then((result) => {
-                res.json(result);
+                res.sendStatus(200);
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Delete Client failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 }
