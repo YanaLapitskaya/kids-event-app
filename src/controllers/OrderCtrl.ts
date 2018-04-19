72ebc5afe1f259b71d0bde778f4efa229fab2ce0
Lapitskaya Yana <Lapitskaya_YV@itd.iba.by>
2018-04-19 18:21:46 +0300

    changed order controller

--- src/controllers/OrderCtrl.ts
+++ src/controllers/OrderCtrl.ts
@@ -10,48 +10,50 @@ export default class OrderRoutes {
     static getAllOrders(req: Request, res: Response) {
         OrderRepo.getAllOrders()
             .then((result) => {
-                res.json(result);
+                res.status(200).send({orders: result.map((order) => order.dataValues)});
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Fetch All Reviews failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static getOrderById(req: Request, res: Response) {
         OrderRepo.getOrderById(req.params.id)
-            .then((result) => res.json(result))
-            .catch((err) => {
-                apiErrorHandler(err, req, res, `Order ${req.params.id} not found.`);
+            .then((order) => {
+                res.status(200).send({order: order});
+            })
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static addOrder(req: Request, res: Response) {
         OrderRepo.createOrder(req.body)
-            .then((result) => {
-                res.json(result);
+            .then((order: any) => {
+                res.status(200).send({order: order});
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Add Order failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static editOrder(req: Request, res: Response) {
         OrderRepo.updateOrder(req.params.id, req.body)
             .then((result) => {
-                res.json(result);
+                res.sendStatus(200);
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Edit Order failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 
     static deleteOrder(req: Request, res: Response) {
         OrderRepo.deleteOrder(req.params.id)
             .then((result) => {
-                res.json(result);
+                res.sendStatus(200);
             })
-            .catch((err) => {
-                apiErrorHandler(err, req, res, "Delete Order failed.");
+            .catch(err => {
+                throw new Error(JSON.stringify(err.message || err));
             });
     }
 }
